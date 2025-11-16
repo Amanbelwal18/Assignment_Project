9f472369c19f76afa1524c809efad944f8f6fc8b
Amanbelwal18 <143030875+Amanbelwal18@users.noreply.github.com>
2025-11-17 00:42:40 +0530

    Add files via upload

---
+++ callbacks.js
@@ -0,0 +1,29 @@
+let output = "hello world";
+
+const posts = [
+    { title: 'Post One', body: 'This is post one' },
+    { title: 'Post Two', body: 'This is post two' }
+];
+
+function getposts() {
+    setTimeout(() => {
+
+        let output = '';
+        posts.forEach((post) => {
+            output += `${post.title}\n`;
+        });
+
+        console.log(output); 
+
+    }, 1000);
+}
+
+function createPost(post ,callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+createPost({ title: 'Post Three', body: 'This is post three' }, getposts);
+
